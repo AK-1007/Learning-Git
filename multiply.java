fc347234a117e973e7ba3c74e7c6fa1808a1f578
Adarsh Kandu <ak_00@Adarshs-MacBook-Air.local>
2024-04-17 15:45:15 +0530

    added message in multiply.java

--- multiply.java
+++ multiply.java
@@ -3,6 +3,6 @@ public class multiply {
     public static void main(String[] args) {
         int a=4;
         int b=5;
-        System.out.println(a*b);
+        System.out.println("multiply is:"+a*b);
     }
 }

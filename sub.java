f3ba960e059d08466ebb30472e8f81d7ccbff7a1
Adarsh Kandu <ak_00@Adarshs-MacBook-Air.local>
2024-04-17 16:10:26 +0530

    added subtraction.java

---
+++ sub.java
@@ -0,0 +1,8 @@
+// import java.util.*;
+public class sub  {
+    public static void main(String[] args) {
+        int a=7;
+        int b=4;
+        System.out.println("subtraction is :"+(a-b));
+    }
+}
